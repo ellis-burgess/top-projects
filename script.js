39b5810af4b0c79152f4cc461823be572fc978cb
El Burgess <elin.burgess@gmail.com>
2022-06-18 14:27:57 +0100

    Add functions for basic operations

---
+++ script.js
@@ -0,0 +1,17 @@
+//functions for add, subtract, divide, and multiply
+
+function addValues(a, b) {
+    return a + b;
+}
+
+function subtractValues(a, b) {
+    return a - b;
+}
+
+function divideValues (a, b) {
+    return a / b;
+}
+
+function multiplyValues(a, b) {
+    return a * b;
+}
