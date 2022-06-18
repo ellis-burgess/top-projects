1accebd540d5e8a6a87bf69b981d891855fee4e9
El Burgess <elin.burgess@gmail.com>
2022-06-18 14:32:10 +0100

    Update operation functions to evaluate strings as numbers

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //functions for add, subtract, divide, and multiply
 
 function addValues(a, b) {
-    return a + b;
+    return Number(a) + Number(b);
 }
 
 function subtractValues(a, b) {
-    return a - b;
+    return Number(a) - Number(b);
 }
 
 function divideValues (a, b) {
-    return a / b;
+    return Number(a) / Number(b);
 }
 
 function multiplyValues(a, b) {
-    return a * b;
+    return Number(a) * Number(b);
 }
