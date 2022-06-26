dc7ed21e9389895a1512d9bb7f4f93fa535929ff
El Burgess <elin.burgess@gmail.com>
2022-06-26 15:11:28 +0100

    Add event listeners to calculator buttons
    Added event listeners to calculator buttons which log to the console
    their value/function.

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
+const display = document.getElementById("lcd-display");
+const clear = document.getElementById("clr");
+const digits = document.querySelectorAll(".digit");
+const operators = document.querySelectorAll(".operator");
+const decimal = document.getElementById("point");
+
+digits.forEach((digit) => {
+    digit.addEventListener('click', () => {
+        console.log(digit.id);
+    })
+})
+
+operators.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        console.log(operator.id);
+    })
+})
+
+clear.addEventListener('click', () => {
+    console.log("Clear");
+})
+
+decimal.addEventListener('click', () => {
+    console.log(".");
+})
+
 //functions for add, subtract, divide, and multiply
-//function for operation
 
 function addValues(a, b) {
     return Number(a) + Number(b);
@@ -16,6 +41,8 @@ function divideValues (a, b) {
 function multiplyValues(a, b) {
     return Number(a) * Number(b);
 }
+
+//function for operation
 
 function operate(a, b, operand) {
     switch (operand) {
