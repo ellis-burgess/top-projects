2657652a3766fbd0104a58880d0e8f529ee033aa
Ellis Burgess <burgessem@cardiff.ac.uk>
2023-01-11 12:41:23 +0000

    Rename calculationArray variable and improve calculation handling

--- script.js
+++ script.js
@@ -6,23 +6,26 @@ const decimal = document.getElementById('.');
 const equals = document.getElementById('=');
 const allButtons = document.querySelectorAll('.btn');
 
-const calculationArray = [];
-let a = 'default';
-let b = 'default';
+const displayArray = [];
+let a, b = 0;
 let operand = 'default';
 let prevButtonClass = '';
+display.textContent = "0";
 
-//monitor for keyboard input
+// event listener for keyboard input
 document.addEventListener('keydown', (e) => {
     allButtons.forEach((button) => {
         if (button.id == e.key) {
             document.getElementById(button.id).click();
         }
     })
+    // treat pressing 'enter' as pressing 'equals'
+    if (e.key === 'Enter') {
+        equals.click();
+    }
 })
 
-display.textContent = "0";
-
+// event listeners for button presses
 digits.forEach((digit) => {
     digit.addEventListener('click', addDigit);
 })
@@ -37,49 +40,46 @@ decimal.addEventListener('click', addDigit);
 
 equals.addEventListener('click', evaluateEquation);
 
-//add digit to array
+// add digit to array
 function addDigit(e) {
-    calculationArray.push(e.target.id);
-    display.textContent = calculationArray.join('');
+    displayArray.push(e.target.id);
+    display.textContent = displayArray.join('');
     prevButtonClass = "digit"
-    console.log(prevButtonClass);
 }
 
-//set operand and current value
+// set operand and current value
 function setOperand(e) {
-    //if previous input was operand
+    // if previous input was operand, overwrite
     if (prevButtonClass == "operand") {
         operand = e.target.id;
         return;
     }
-    //if there is an unevaluated equation
+    // if there is an unevaluated equation
     if (operand != 'default') {
         display.textContent = operate(a, Number(display.textContent), operand);
     }
-    //standard process
+    // set operand
     operand = e.target.id;
     a = Number(display.textContent);
-    calculationArray.length = 0;
+    displayArray.length = 0;
     prevButtonClass = "operand";
-    console.log(prevButtonClass);
 }
 
-//set second value and evaluate equation on equals
+// set second value and evaluate equation on equals
 function evaluateEquation() {
+    // set b and evaluate equation
     b = Number(display.textContent);
     display.textContent = operate(a, b, operand);
-    a = 'default';
-    b = 'default';
+    // reset all values
+    a = b = 0;
     operand = 'default';
-    calculationArray.length = "0";
+    displayArray.length = 0;
     prevButtonClass = "equals"
-    console.log(prevButtonClass);
-
 }
 
-//functional clear button
+// functional clear button
 function clearCalculator() {
-    //C vs AC
+    // C vs AC
     if ((prevButtonClass == "clr") || (prevButtonClass == "operand")) {
 
         console.log(prevButtonClass);
@@ -96,40 +96,25 @@ function clearCalculator() {
             prevButtonClass = "clr"
             return;
     }
-    calculationArray.length = "0";
+    displayArray.length = 0;
     display.textContent = "0";
 }
 
-//functions for add, subtract, divide, and multiply
-
-function addValues(a, b) {
-    return Number(a) + Number(b);
-}
-
-function subtractValues(a, b) {
-    return Number(a) - Number(b);
-}
-
-function divideValues(a, b) {
-    return Number(a) / Number(b);
-}
-
-function multiplyValues(a, b) {
-    return Number(a) * Number(b);
-}
-
-//function for operation
+// function for operation
 
 function operate(a, b, operand) {
     switch (operand) {
         case '+':
-            return addValues(a, b);
+            return Number(a) + Number(b);
         case '-':
-            return subtractValues(a, b);
+            return Number(a) - Number(b);
         case '/':
-            return divideValues(a, b);
+            if (Number(b) === 0) {
+                return 'Error'
+            }
+            return Number(a) / Number(b);
         case '*':
-            return multiplyValues(a, b);
+            return Number(a) * Number(b);
         default:
             return 'Error';
     }
