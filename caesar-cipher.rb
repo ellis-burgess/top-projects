c2c02be81f177b1fc5aeb2dd8f2ccfbf6e50f97f
Ellis Burgess <burgessem@cardiff.ac.uk>
2023-06-10 18:46:08 +0200

    Add error handling for invalid input types

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,4 +1,14 @@
 def caesar_cipher(str, offset)
+  unless str.respond_to? :get_byte
+    raise TypeError,
+      "cannot encode object of type #{ str.class }"
+  end
+
+  unless offset.is_a?(Integer)
+    raise TypeError,
+      "Can only use integer for a caesar cipher"
+    end
+
   offset = offset % 26 if offset > 26
   i = 0
   lowers = 97..122
