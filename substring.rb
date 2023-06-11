4abd4595f27275ed75be1255a9804a5d52b53d8c
Ellis Burgess <burgessem@cardiff.ac.uk>
2023-06-11 14:25:22 +0200

    Rewrote error message for invalid str type

--- substring.rb
+++ substring.rb
@@ -1,7 +1,7 @@
 def substrings(str, dictionary)
   unless str.is_a?(String) || str.is_a?(Array)
     raise TypeError,
-      "Invalid type. Please use a string or an array."
+      "Can only search for substrings in a string or array."
   end
 
   if str.is_a?(Array) && str.any? { |word| !(word.is_a?(String)) }
@@ -17,9 +17,11 @@ def substrings(str, dictionary)
   substrings_found = Hash.new(0)
   str = str.dup.join if str.is_a?(Array)
   str = str.downcase
+
   dictionary.each do |word|
     word = word.downcase
     str.scan(word) { |word| substrings_found[word] += 1 }
   end
+
   substrings_found
 end
