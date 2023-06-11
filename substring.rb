dea98a3f6a47eb2391bfdbb7bb208af707aa8c98
Ellis Burgess <burgessem@cardiff.ac.uk>
2023-06-11 14:15:32 +0200

    Add error handling and array input handling

--- substring.rb
+++ substring.rb
@@ -1,23 +1,20 @@
 def substrings(str, dictionary)
+  unless str.is_a?(String) || str.is_a?(Array)
+    raise TypeError,
+      "Invalid type. Please use a string or an array."
+  end
+
+  unless dictionary.is_a?(Array)
+    raise TypeError,
+      "Please only use an array for your dictionary"
+  end
+
   substrings_found = Hash.new(0)
-  str = str.dup.downcase
+  str = str.dup.join if str.is_a?(Array)
+  str = str.downcase
   dictionary.each do |word|
     word = word.downcase
     str.scan(word) { |word| substrings_found[word] += 1 }
   end
   substrings_found
 end
-
-dictionary = ["BELOW","down","go","going","horn","how","howdy","it","i","low",
-  "own","part","partner","sit"]
-p substrings("below", dictionary)
-# Expect return: { "below" => 1, "low" => 1 }
-
-p substrings("Howdy partner, sit down! How's it going?", dictionary)
-# Expected:
-# { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2,
-# "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
-
-##
-# Go through str, testing each word in dictionary
-# Each time word is in str, words[word] += 1
